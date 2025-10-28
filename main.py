e159fd3ef6c6cfdf5206f0069e286db9c2db3725
minmin2104 <abeamin23@gmail.com>
2025-10-28 14:59:58 +0800

    add opencv and load data

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import cv2
+import numpy as np
 import zipfile
 
 
@@ -11,8 +14,31 @@ def unzip_content():
     print("datasets unzip successfully")
 
 
+def load_data_and_labels():
+    categories = ["cats_set", "dogs_set"]
+    data = []
+    labels = []
+    for ct in categories:
+        path = os.path.join(EXTRACT_PATH, ct)
+        for img_file in os.listdir(path):
+            img_path = os.path.join(path, img_file)
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+            if img is None:
+                continue
+            img = cv2.resize(img, (32, 32))
+            data.append(img)
+            labels.append(0 if ct == "cats_set" else 1)
+    data = np.array(data)
+    labels = np.array(labels)
+
+    data = data / 255.0
+    print("Data loaded successfully!")
+    print("Data shape:", data.shape)
+    print("Label shape:", labels.shape)
+    return data, labels
+
 def main():
-    pass
+    data, label = load_data_and_labels()
     
 
 if __name__ == "__main__":
